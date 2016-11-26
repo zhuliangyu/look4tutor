acfa30bc00037325a8b62375c36e860bdfaf9ec8
RickyZhu <riozhu@gmail.com>
2016-11-25 22:07:20 -0800

    add star for comment rating

---
+++ app/assets/javascripts/star.js
@@ -0,0 +1,51 @@
+/**
+ * Created by macbook on 2016-11-25.
+ */
+<!-- Latest compiled and minified JavaScript -->
+
+$('#comment-form').ready(
+    function () {
+
+        $("#rateYo").rateYo({
+            rating: 3.6
+        });
+
+        $("#rateYo").click(function () {
+            $('#comment_rate').val($("#rateYo").rateYo("rating"))
+
+        })
+
+    }
+);
+
+
+// $(document).ready(
+//     function () {
+//
+//         $("#rateYo").rateYo({
+//             rating: 3.6
+//         });
+//
+//         $("#rateYo").click(function () {
+//             $('#comment_rate').val($("#rateYo").rateYo("rating"))
+//
+//         })
+//
+//     }
+//
+//
+// );
+
+
+// $(function () {
+//
+//     $("#rateYo").rateYo({
+//         rating: 3.6
+//     });
+//
+//     $("#rateYo").click(function () {
+//         $('#comment_rate').val($("#rateYo").rateYo("rating"))
+//
+//     })
+//
+// });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -79,6 +79,8 @@ class CommentsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.require(:comment).permit(:rate, :body)
+
+
   end
 
 
