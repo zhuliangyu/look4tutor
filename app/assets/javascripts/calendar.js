96f3f0e98121f7e80319e124a0d155e433844643
RickyZhu <riozhu@gmail.com>
2016-11-26 23:37:58 -0800

    modify calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -4,26 +4,43 @@
 
 
 var calendar=function() {
-    $('#calendar').fullCalendar({});
+     $('#calendar').fullCalendar({
+         header: {
+             left: 'prev,next today',
+             center: 'title',
+             right: 'month,basicWeek, basicDay'
+         },
+         dayClick: function(date, jsEvent, view) {
+             var ajandamodu=view.name;
+             if(ajandamodu=='month')
+             {
+                 $('#calendar').fullCalendar( 'changeView', 'listDay'  );
+             }
+         }
+     });
+
+    // $('#calendar').fullCalendar('next');
 
 
-    $("#calendar").fullCalendar({
-        header:"My calendar",
-        left: "prev,next today",
-        center: "title",
-        right: "month,agendaWeek,agendaDay",
-        defaultView: "month",
-        height: 500,
-        slotMinutes: 15,
-        events: "/dashboard/get_events",
-        timeFormat: "h:mm t{ - h:mm t} ",
-        dragOpacity: "0.5"
-    });
+
+
+
+    // $("#calendar").fullCalendar({
+    //     header:"My calendar",
+    //     left: "prev,next today",
+    //     center: "title",
+    //     right: "month,agendaWeek,agendaDay",
+    //     defaultView: "month",
+    //     height: 500,
+    //     slotMinutes: 15,
+    //     events: "/dashboard/get_events",
+    //     timeFormat: "h:mm t{ - h:mm t} ",
+    //     dragOpacity: "0.5"
+    // });
 };
 
 
-// $(document).ready(ready)
-// $(document).on('page:load', ready)
+
 document.addEventListener("turbolinks:load", function() {
     calendar();
 });
