df2e1a2d60f7b6672d54869d2d6e15560485070c
Ricky Zhu <ricky@enbut.com>
2016-12-04 23:45:55 -0800

    ajax comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,6 +36,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
+
         format.html { redirect_to @tutor, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
