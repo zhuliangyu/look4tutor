71c5bae7aceace0a06d7a379928eabed1f37faa2
Ricky Zhu <ricky@enbut.com>
2016-12-06 10:32:44 -0800

    add password comfirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :is_tutor, :is_admin)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,:is_tutor, :is_admin)
   end
 end
 
