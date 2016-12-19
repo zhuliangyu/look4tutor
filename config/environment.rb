5f2982aae16c6c29b78a16ea52f4f1c82c0c1deb
Ricky Zhu <ricky@enbut.com>
2016-12-19 01:03:47 -0800

    change host

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@ require 'will_paginate'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'look4tutor.heroku.com' }
+config.action_mailer.default_url_options = { :host => 'https://look4tutor.herokuapp.com' }
 
