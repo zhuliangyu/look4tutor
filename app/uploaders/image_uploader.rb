4c7084ffa5cd0089e75a625507f5a23d44fdef65
Ricky Zhu <ricky@enbut.com>
2016-12-23 19:40:54 -0700

    set cache for pictures uploading

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
